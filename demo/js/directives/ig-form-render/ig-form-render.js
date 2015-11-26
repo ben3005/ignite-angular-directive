9ea61941d904687d12f0393467c7044c6df83a07
Ben Marshall <ben3005@hotmail.com>
2015-11-26 20:03:13 +0000

    added ioninc project, added a slide box

--- demo/js/directives/ig-form-render/ig-form-render.js
+++ demo/js/directives/ig-form-render/ig-form-render.js
@@ -3,15 +3,25 @@
 
     var directives = angular.module('directive-test.directives');
 
-    directives.directive('igFormRender', function () {
+    directives.directive('igFormRender', ['$ionicSlideBoxDelegate', '$timeout', function ($ionicSlideBoxDelegate, $timeout) {
         var directiveDefinition = {
             restrict: 'E',
             scope: {
                 form: '='
             },
             link: function (scope, element, attrs, controller, transcludeFn) {
+                scope.$on('$ionicView.enter', function () {
+                    $timeout(function () {
+                        $ionicSlideBoxDelegate.$getByHandle('ig-form-slide-box').update();
+                    }, 0)
+                });
 
+                scope.currentSlideIndex = 0;
 
+                scope.updateSlideIndex = function ($index) {
+                    console.log($index);
+                    scope.currentSlideIndex = $index;
+                };
                 // From angular docs, Best Practice to clean up when directive is removed
                 scope.$on('$destroy', function () {
 
@@ -20,5 +30,5 @@
             templateUrl: 'js/directives/ig-form-render/ig-form-render.html'
         };
         return directiveDefinition;
-    });
+    }]);
 })();
